d48b319427991462b648ec8371473bf400c2de95
Binnik1409 <brynjarh23@ru.is>
2025-11-24 13:04:36 +0000

    V2

--- Verkefni 1/V1-2.py
+++ Verkefni 1/V1-2.py
@@ -1,13 +1,17 @@
 import sys
-sys.path.append('..')
+import os
+import numpy as np
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from newton import newton
 
 f = lambda s: 2*s**4 + 23*s**3 + 45*s**2 + 60*s + 50
 Df = lambda s: 8*s**3 + 69*s**2 + 90*s + 60
 
-punktar = [-9.45, -1.4]
+p = 1j
 tol = 10**(-3)
 
-for i in punktar:
-    x = newton(i, tol, f, Df)
-    print(x)
+x1 = newton(p, tol, f, Df)
+print(x1)
+
+x2 = np.conj(x1)
+print(x2)
