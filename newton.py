63a19a3d9fc613847833d09f3f9132fbfae24ebe
Binnik1409 <brynjarh23@ru.is>
2025-11-24 13:41:59 +0000

    tafla með samleitni og villur

--- newton.py
+++ newton.py
@@ -1,8 +1,10 @@
 def newton(x0,tol, f, Df):
     oldx=x0+2*tol
     x=x0
+    errors = []
     while abs(oldx-x)>tol:
         oldx=x
         x=x-f(x)/Df(x)
-    return(x)
+        errors.append(abs(oldx-x))
+    return(x, errors)
 
