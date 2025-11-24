0740545f7b33fe078edf3392e5896470c8be08a9
Binnik1409 <brynjarh23@ru.is>
2025-11-24 02:26:54 +0000

    Initial commit: Verkefni 1 solution with Icelandic comments

---
+++ Verkefni 1/V1-1.py
@@ -0,0 +1,31 @@
+# Fyrst þurfum við að skilgreina a, b og fallið
+
+a = -9.45
+b = -9.4
+
+f = lambda s: 2*s**4 + 23*s**3 + 45*s**2 + 60*s + 50
+
+# Því næst þurfum við að skilgreina breyturnar fyrir helmingunaraðferðina
+
+n = 0                           # n er fjöldi ítrekana
+E = (b-a)/(2**n)                # E er skekkjan
+target_E = (1*10**(-10))        # target_E er markmiðsskekkjan
+
+# Þá þurfum við að skilgreina while lykkjuna.
+# While lykkjan heldur áfram þar til skekkjan er minni en eða jafnt og markmiðsskekkjan.
+
+while E >= target_E:
+    c = (a+b)/2                 # c er miðpunktur a og b
+    
+    if f(c) == 0:               # Ef fallgildið í c er 0, þá er c rótin
+        break   
+    
+    if f(a)*f(c) < 0:           # Ef fallgildin í a og c hafa mismunandi formerki, þá er rótin á milli a og c
+        b = c
+    else:                       # Annars er rótin á milli c og b
+        a = c
+    
+    n = n+1                     # Hækka fjölda ítrekana
+    E = (b-a)/(2**n)            # Uppfæra skekkjuna
+
+print(c)                        # Prenta rótina
