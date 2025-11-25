2c2517fd798ba41c660c8eb398cee3161e942eab
thefinaltry <finnurhugi@gmail.com>
2025-11-25 10:01:14 +0000

    starting V1-3.py

--- Verkefni 1/V1-3.py
+++ Verkefni 1/V1-3.py
@@ -1,2 +1,10 @@
 import matplotlib as plt
 
+# Fall sem tekur inn C1 (Dempunnarstuðull 1) sem breytu og skila rauntöluhluta tvinntölulausnar
+def breytanleg_dempun(c1):
+
+
+
+
+# Skilgreining á 
+C_1 = [i for i in range(1,30,1)]
