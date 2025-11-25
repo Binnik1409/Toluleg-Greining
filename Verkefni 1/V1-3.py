163ab9990be19701ba8439cbd45a8b30cd563054
thefinaltry <finnurhugi@gmail.com>
2025-11-25 10:45:05 +0000

    changes to requirements.txt, numpy to np V1-4.py

--- Verkefni 1/V1-3.py
+++ Verkefni 1/V1-3.py
@@ -6,5 +6,10 @@ def breytanleg_dempun(c1):
 
 
 
-# Skilgreining á 
+# Breytur og fastar
 C_1 = [i for i in range(1,30,1)]
+Raun_gildi = []
+
+for c1 in C_1:
+    Raun_gildi.append(breytanleg_dempun(c1))
+
