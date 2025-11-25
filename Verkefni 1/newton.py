02970e0267528158b7d9e59e51cbebfae1defcb8
Binnik1409 <brynjarh23@ru.is>
2025-11-25 10:24:47 +0000

    gerði betra newton

--- Verkefni 1/newton.py
+++ Verkefni 1/newton.py
@@ -1,10 +1,16 @@
-def newton(x0,tol, f, Df):
+def newton(x0,tol, f, Df, errors = False):
     oldx=x0+2*tol
     x=x0
-    errors = []
-    while abs(oldx-x)>tol:
-        oldx=x
-        x=x-f(x)/Df(x)
-        errors.append(abs(oldx-x))
-    return x, errors
+    if errors:
+        errors = []
+        while abs(oldx-x)>tol:
+            oldx=x
+            x=x-f(x)/Df(x)
+            errors.append(abs(oldx-x))
+        return x, errors
+    else:
+        while abs(oldx-x)>tol:
+            oldx=x
+            x=x-f(x)/Df(x)
+        return x
 
