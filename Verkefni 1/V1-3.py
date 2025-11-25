81e3c9af3626292c39f3a24ab9315f17f62e73b7
Binnik1409 <brynjarh23@ru.is>
2025-11-25 11:55:50 +0000

    mjá

--- Verkefni 1/V1-3.py
+++ Verkefni 1/V1-3.py
@@ -1,2 +1,18 @@
-import matplotlib as plt
+import numpy as np
+import matplotlib.pyplot as plt
+from f import f
 
+s = complex(-0.35342173481620454, -1.3443489177880488)
+c = np.linspace(0, 30, 1000)
+
+func = []
+for j in c:
+    func.append(f(s,c1 = j))
+
+# fig, ax = plt.subplots()
+# ax.plot(c, func)
+# ax.grid()
+# plt.show()
+
+for i in func:
+    print(i, end="\n")
