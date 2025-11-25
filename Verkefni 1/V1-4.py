dd11d94de3920307a669270ced04a0097038dab2
JBTmarel <a.orribjarnason@gmail.com>
2025-11-25 10:12:46 +0000

    dæmi 4

--- Verkefni 1/V1-4.py
+++ Verkefni 1/V1-4.py
@@ -1,14 +1,15 @@
-import numpy
+import numpy as np
 import matplotlib as plt
 import newton
 import f
 import Df
 
+ROUND = 5
 TOL = 10**(-14)
 
 def make_grid(R_low, R_high, I_low, I_high, N):
-    R = numpy.linspace(R_low, R_high, N)
-    I = numpy.linspace(I_low, I_high, N)
+    R = np.linspace(R_low, R_high, N)
+    I = np.linspace(I_low, I_high, N)
     grid = []
     for i in R:
         grid.append([])
@@ -24,4 +25,16 @@ for x in grid:
     for y in x:
         results[-1].append(newton.newton(y, TOL, f.f, Df.Df))
 
-results = [round(x, 5) for x in results]
+all_results = []
+
+for x in results:
+    for y in x:
+        all_results.append(np.round(y, ROUND))
+
+print(len(all_results))
+all_results = list(set(all_results))
+
+print(len(all_results))
+
+for item in all_results:
+    print(item)
