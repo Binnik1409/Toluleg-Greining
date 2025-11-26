81e066610e5363c38d35af74c1133b87478bcbcb
Binnik1409 <brynjarh23@ru.is>
2025-11-26 23:54:36 +0000

    Gerði dæmi 4 næstum ☺

--- Verkefni 2/V2-4.py
+++ Verkefni 2/V2-4.py
@@ -1,59 +1,88 @@
 import numpy as np
 
 class Vector:
-    def __init__(self, vector):
+    def __init__(self, vector, jacob = None):
         self.vector = vector
         self.size = len(vector)
+        self.jacob = jacob
     def jacobian(self):
+        if self.jacob:
+            return self.jacob
         jacobian = [[[] for _ in range(self.size)] for _ in range(self.size)]
         for i in range(self.size):
             for j in range(self.size):
-                jacobian[i][j] = [self.vector[i][j][0] * self.vector[i][j][1], self.vector[i][j][1] - 1]
+                if self.vector[i][j][1] == 0:
+                    jacobian[i][j] = [0, 0]
+                else:
+                    jacobian[i][j] = [self.vector[i][j][0] * self.vector[i][j][1], self.vector[i][j][1] - 1]
+        self.jacob = jacobian
         return jacobian
     def solve(self, vals):
         solved = []
         for i in range(self.size):
             temp = self.vector[i][self.size]
             for j in range(self.size):
-                temp += self.vector[i][j][0] * vals[j] ** self.vector[i][j][1]
+                temp = temp + self.vector[i][j][0] * vals[j] ** self.vector[i][j][1]
             solved.append(temp)
         return solved
     def solve_jacobian(self, vals):
         solved = [[[] for _ in range(self.size)] for _ in range(self.size)]
         for i in range(self.size):
             for j in range(self.size):
-                solved[i][j] = self.jacobian()[i][j][0] * vals[j] ** self.jacobian()[i][j][1]
+                if vals[j] == 0:
+                    solved[i][j] = vals[j]
+                else:
+                    solved[i][j] = self.jacobian()[i][j][0] * vals[j] ** self.jacobian()[i][j][1]
         return solved
     def solve_s(self, vals):
         return np.linalg.solve(self.solve_jacobian(vals), self.solve(vals))
+    def print_vector(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                print(self.vector[i][j], end="  \t")
+            print(self.vector[i][self.size], end="")
+            print()
+    def print_jacobian(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                print(self.jacobian()[i][j], end="  \t")
+            print()
 
+QB = 1
+p0 = 0
+p1 = 2
+K = 1
 
-# def vector():
-#     a = [[2, 1], [2, 3], [2, 4], 3]
-#     b = [[1, 2], [4, 1], [2, 1], 0]
-#     c = [[4, 3], [8, 4], [0, 0], -14]
-#     return [a, b, c]
+vector = [
+    [[1,1], [-1,1], [-1,1], [0,0], [0,0], [0,0], [0,0], [0,0], 0],
+    [[0,0], [1,1], [0,0], [-1,1], [0,0], [0,0], [0,0], [0,0], QB],
+    [[0,0], [0,0], [1,1], [0,0], [-1,1], [-1,1], [0,0], [0,0], 0],
+    [[0,0], [0,0], [0,0], [1,1], [1,1], [0,0], [-1,1], [0,0], 0],
+    [[0,0], [0,0], [0,0], [0,0], [0,0], [1,1], [1,1], [-1,1], 0],
+    [[0,0], [1,2], [-1,2], [1,2], [-1,2], [0,0], [0,0], [0,0], 0],
+    [[0,0], [0,0], [0,0], [0,0], [1,2], [-1/2,2], [3/2,2], [0,0], 0],
+    [[1,2], [0,0], [1,2], [0,0], [0,0], [1/2,2], [0,0], [1,2], (p0-p1)/K]
+]
 
-# def s_func(vector, jacobian):
-#     s = 0
-#     for i in range(len(vector)):
-#         for j in range(len(vector)):
-#             s += vector[i][j] * jacobian[i][j]
-#     return s
+x0 = [1,1,2,3,4,3,2,1]
+Vector(vector).print_vector()
+print()
+Vector(vector).print_jacobian()
+print()
 
-def vector():
-    a = [[2, 1], [2, 3], 3]
-    b = [[1, 2], [4, 1], -2]
-    return [a, b]
+tol = 10**(-8)
+nakv = int(np.ceil(-np.log10(tol)+2))
+E = tol + 1
+while E > tol:
+    x = x0 - Vector(vector).solve_s(x0)
+    E = np.linalg.norm(x - x0)
+    x0 = x
+    for i in x:
+        print("%.*f" % (nakv, i), end="  ")
+    print()
 
-print(vector())
-print(Vector(vector()).jacobian())
-print(Vector(vector()).solve([1, 2]))
-print(Vector(vector()).solve_jacobian([1, 2]))
-
-# def jacobian(vector):
-#     jacobian = [[[] for _ in range(len(vector))] for _ in range(len(vector))]
-#     for i in range(len(vector)):
-#         for j in range(len(vector)):
-#             jacobian[i][j] = [vector[i][j][0] * vector[i][j][1], vector[i][j][1] - 1]
-#     return jacobian
+# print(vector)
+# print(Vector(vector).jacobian())
+# print(Vector(vector).solve(x0))
+# print(Vector(vector).solve_jacobian(x0))
+# print(Vector(vector).solve_s(x0))
