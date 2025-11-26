7b4481919a654837b3b950b6ea02d83c7ab42796
thefinaltry <finnurhugi@gmail.com>
2025-11-26 13:27:16 +0000

    Fix V2-1.py ^ to ** and forgotten -

--- Verkefni 2/V2-1.py
+++ Verkefni 2/V2-1.py
@@ -3,16 +3,16 @@ from numpy import linalg as la
 import math as m
 
 # Fastar #
-v = 1*10^-3
-r = 5*10^-2
+v = 1*10**(-3)
+r = 5*10**(-2)
 L = 100
-G = (m.pi*(r^4))/(8*v*L)
-p1 = 4.2 * (10^6)
+G = (m.pi*(r**4))/(8*v*L)
+p1 = 4.2*10**(6)
 p0 = 0
 Q_b = 7
 
 # Skilgreining á fyljunum a og b fyrir ax = b
-A = np.matrix('3 1 1 0 0; 1 2 0 1 0; 1 0 -4 1 2; 0 3 3 -8 2; 0 0 6 2 -11')
+A = np.matrix('3 -1 -1 0 0; 1 -2 0 1 0; 1 0 -4 1 2; 0 3 3 -8 2; 0 0 6 2 -11')
 B = np.matrix(f'{p1};{-Q_b/G}; 0; 0; 0')
 
 # Nota linalg til að leysa fyrir x
@@ -35,5 +35,5 @@ flow = [g * (i - j) for g, i, j in poisuilles_gildi] #[q_1A, q_AB, q_AC, q_BD, q
 r = '  '.join(map(str, flow)).replace("[[","").replace("]]","")
 
 # Print
-print('q_1A         q_AB         q_AC         q_BD         q_CD         q_CE         q_DE         q_E0')
+print('q_1A         q_AB         q_AC         q_BD          q_CD        q_CE        q_DE         q_E0')
 print(r)
