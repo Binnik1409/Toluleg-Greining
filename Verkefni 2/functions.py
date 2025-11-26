4bc60a0ab6923bf02884028eaabd84c73019abad
thefinaltry <finnurhugi@gmail.com>
2025-11-26 09:33:23 +0000

    Adding basic functions to functions.py for V2

---
+++ Verkefni 2/functions.py
@@ -0,0 +1,61 @@
+import math
+
+
+
+def bisection(f,a,b,tol):
+    '''gert ráð fyrir að búið se að skilgreina f(x) fyrir utan t.d.
+    def f(x):
+        return(x**2-2)
+    '''
+    if f(a)*f(b) >= 0:
+        print("Bisection method fails.")
+        return None
+    else:
+        fa=f(a)
+        while (b-a)/2>tol:
+            c=(a+b)/2
+            fc=f(c)
+            if fc==0:break
+            if fc*fa<0:
+                b=c
+            else:
+                a=c
+                fa=fc
+    return((a+b)/2)
+
+def goldensearch(a,b,tol,f):
+    phi=(math.sqrt(5)-1)/2
+    x1=a+(1-phi)*(b-a)
+    x2=a+phi*(b-a)
+    f1=f(x1)
+    f2=f(x2)
+    while (b-a)/2>tol:
+        if f1<f2:
+            b=x2
+            x2=x1
+            x1=a+(1-phi)*(b-a)
+            f2=f1
+            f1=f(x1)
+        else:
+            a=x1
+            x1=x2
+            x2=a+phi*(b-a)
+            f1=f2
+            f2=f(x2)
+    return((a+b)/2)
+
+def newton(x0,tol, f, Df, errors = False):
+    oldx=x0+2*tol
+    x=x0
+    if errors:
+        errors = []
+        while abs(oldx-x)>tol:
+            oldx=x
+            x=x-f(x)/Df(x)
+            errors.append(abs(oldx-x))
+        return x, errors
+    else:
+        while abs(oldx-x)>tol:
+            oldx=x
+            x=x-f(x)/Df(x)
+        return x
