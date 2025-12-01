0f12f1308ad9d689a0d841a1699098b7b9b72a11
Binnik1409 <brynjarh23@ru.is>
2025-12-01 13:25:25 +0000

    asdf

--- Verkefni 3/Hluti 1/V3-2.py
+++ Verkefni 3/Hluti 1/V3-2.py
@@ -1,13 +1,19 @@
+import math as m
+
 def eulerstep(t, x, h):
-    return x+h*ydot(t,x)
+    return [j+h*i for j,i in zip(x,ydot(t,x))]
 
 def ydot(t, x):
     g, L = 9.81, 2
     x_ = x[1]
-    y_ = -(g/L)*sin(x[0])
+    y_ = -(g/L)*m.sin(x[0])
     return [x_, y_]
 
-h = 0.01
+n = 10
+t = 1
+h = t/n
+x = [20, 0]
 
-for i in range(1000):
-    a, b = eulerstep(, h)
+for i in range(n):
+    x = eulerstep(t, x, h)
+    print(x)
