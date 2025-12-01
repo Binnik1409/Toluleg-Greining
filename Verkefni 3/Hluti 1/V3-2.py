8bac9f5a00904c30dff82e70874041c7a8cf37cc
Binnik1409 <brynjarh23@ru.is>
2025-12-01 12:57:48 +0000

    oælkj

--- Verkefni 3/Hluti 1/V3-2.py
+++ Verkefni 3/Hluti 1/V3-2.py
@@ -1,7 +1,13 @@
 def eulerstep(t, x, h):
     return x+h*ydot(t,x)
+
 def ydot(t, x):
     g, L = 9.81, 2
-    return [x2, -(g/L)*sin(x1)]
+    x_ = x[1]
+    y_ = -(g/L)*sin(x[0])
+    return [x_, y_]
 
 h = 0.01
+
+for i in range(1000):
+    a, b = eulerstep(, h)
