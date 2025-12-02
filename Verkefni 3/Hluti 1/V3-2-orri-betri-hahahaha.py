e942bf9d2b6595f8bbb23c99f521318da53cd9a8
JBTmarel <a.orribjarnason@gmail.com>
2025-12-02 13:19:54 +0000

    jsdh fioruehfiaehgiosuh oihdfgoiae fh

--- Verkefni 3/Hluti 1/V3-2-orri-betri-hahahaha.py
+++ Verkefni 3/Hluti 1/V3-2-orri-betri-hahahaha.py
@@ -1,5 +1,27 @@
-def eulerstep(t, x, h):
-    return x+h*ydot(t,x)
-def ydot(t, x):
+import math as m
+import numpy as np
+
+def eulerstep(y, h, f):
+    return [y[0]+y[1]*h, f(y[0])]
+
+def ydot(y):
     g, L = 9.81, 2
-    return [x2, -(g/L)*sin(x1)]
+    return [y[1], -(g/L)*m.sin(y[0])]
+
+T = 30
+n = 500
+h = T/n
+
+y0 = [20]
+w0 = [2]
+
+y0.extend(np.zeros(n-1))
+w0.extend(np.zeros(n-1))
+
+
+y = np.matrix([y0, w0])
+
+for i in range(n):
+    y[:,i+1] = eulerstep(y[:,i], h, ydot)
+
+np.plot([i for i in n], y[0])
