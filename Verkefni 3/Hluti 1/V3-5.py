f4f45eb3c790bcc0a3a4da1596c7303d11dc7618
thefinaltry <finnurhugi@gmail.com>
2025-12-03 12:37:54 +0000

    g

--- Verkefni 3/Hluti 1/V3-5.py
+++ Verkefni 3/Hluti 1/V3-5.py
@@ -21,39 +21,39 @@ solution_1 = RKsolverLotkaVolterra(y0, T, n, f)
 y0 = [m.pi/2, 0]
 solution_2 = RKsolverLotkaVolterra(y0, T, n, f)
 
+for i,sol in enumerate([solution_1,solution_2]):
+    # figure
+    plt.close("all")
+    fig = plt.figure(i+1)
 
-# figure
-plt.close("all")
-fig = plt.figure()
+    # subplot
+    # Stilla ása
+    pad = 0
+    ax1 = fig.add_subplot(111, autoscale_on=False, xlim=(0.0-pad, 15.0+pad), ylim=(-10-
+    pad, 10+pad))
+    plt.xlabel('x [eining]')
+    plt.ylabel('y [eining]')
 
-# subplot
-# Stilla ása
-pad = 0
-ax1 = fig.add_subplot(111, autoscale_on=False, xlim=(0.0-pad, 15.0+pad), ylim=(-10-
-pad, 10+pad))
-plt.xlabel('x [eining]')
-plt.ylabel('y [eining]')
+    # layout to tight
+    fig.tight_layout()
 
-# layout to tight
-fig.tight_layout()
+    # Ein lota
+    T = 2.0*np.pi
 
-# Ein lota
-T = 2.0*np.pi
+    FPS = 30 # Fjöldi ramma á sek í hreyfimyndinni.
+    # Til að hægja á hreyfimyndinni er hægt að auka FPS og halda FPS_PLAY föstu.
+    FPS_PLAY = 30 # Fjöldi ramma á sek þegar við spilum hreyfimyndinna.
+    # Best að hafa það 30.
 
-FPS = 30 # Fjöldi ramma á sek í hreyfimyndinni.
-# Til að hægja á hreyfimyndinni er hægt að auka FPS og halda FPS_PLAY föstu.
-FPS_PLAY = 30 # Fjöldi ramma á sek þegar við spilum hreyfimyndinna.
-# Best að hafa það 30.
+    t_start = 0.0 # Byrjunar tími í sek
+    t_end = 2*T # Enda tími í sek (Tvær lotur)
 
-t_start = 0.0 # Byrjunar tími í sek
-t_end = 2*T # Enda tími í sek (Tvær lotur)
-
-# Reikna út fjölda ramma sem þarf og stærð eins tíma skrefs.
-n = int(np.ceil(FPS*(t_end - t_start)))
-dt = (t_end - t_start)/n
+    # Reikna út fjölda ramma sem þarf og stærð eins tíma skrefs.
+    n = int(np.ceil(FPS*(t_end - t_start)))
+    dt = (t_end - t_start)/n
 
 
-for sol in [solution_1,solution_2]:
+
     # Gögn
     line_1, = ax1.plot(sol[0,:], sol[1,:], 'b-', ms=6) # x og y
     t = np.zeros(n) # Tími
@@ -78,8 +78,8 @@ for sol in [solution_1,solution_2]:
 
     # Sýna plot gluggann
     plt.show()
-
+    
     # Búa til skránna
     Writer = animation.writers['ffmpeg']
     writer = Writer(fps=FPS_PLAY, metadata=dict(artist='Me'), bitrate=1800)
-    anim.save('vid.mp4', writer=writer)
+    anim.save(f'{i+1}vid.mp4', writer=writer)
