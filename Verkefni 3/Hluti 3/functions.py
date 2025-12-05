d3c6b349cc6e74c4089b34809f4e0e80e1dd7284
thefinaltry <finnurhugi@gmail.com>
2025-12-05 21:20:52 +0000

    Samræma func filana

--- Verkefni 3/Hluti 3/functions.py
+++ Verkefni 3/Hluti 3/functions.py
@@ -11,7 +11,7 @@ def RKsolverLotkaVolterra(y0, T, n, f):
     theta = [y0[0]]
     omega = [y0[1]]
 
-    for i in range(n):
+    for _ in range(n):
         k1 = f(y)
         k2 = f(y + 0.5*h*k1)
         k3 = f(y + 0.5*h*k2)
@@ -54,84 +54,15 @@ def f(y):
     return np.array([d_theta,d_omega])
 
 
-def make_plt(x, y, theta):
-
-    # figure
-    plt.close("all")
-    fig = plt.figure()
-
-    # subplot
-    # Stilla ása
-    pad = 0
-    ax1 = fig.add_subplot(111, autoscale_on=False, xlim=(-2.5-pad, 2.5+pad), ylim=(-2.5-pad, 2.5+pad), aspect='equal')
-    plt.xlabel('Lengdareining')
-    plt.ylabel('Lengdareining')
-
-    # layout to tight
-    fig.tight_layout()
-
-    FPS = 30 # Fjöldi ramma á sek í hreyfimyndinni.
-    # Til að hægja á hreyfimyndinni er hægt að auka FPS og halda FPS_PLAY föstu.
-    FPS_PLAY = 30 # Fjöldi ramma á sek þegar við spilum hreyfimyndinna.
-    # Best að hafa það 30.
-
-    t_start = 0.0 # Byrjunar tími í sek
-    t_end = 2*2*np.pi # Enda tími í sek (Tvær lotur)
-
-    # Reikna út fjölda ramma sem þarf og stærð eins tíma skrefs.
-    n = int(np.ceil(FPS*(t_end - t_start)))
-    dt = (t_end - t_start)/n
-
-    # Gögn
-    line_1, = ax1.plot(x, y, 'b-', ms=6) # x og y
-    t = np.zeros(n) # Tími
-
-    circle = ax1.add_patch(
-        plt.Circle([-1000, -1000], 0.1, fc="r", zorder=3)
-    )
-
-    def init():
-        line_1.set_data([], [])
-        return line_1, # Passa að komman þarf að vera
-
-    # Teikna
-    def animate(i):
-
-        # Reikna tímann
+def f2(start, m1=1, m2=1, l1=2, l2=2, g=9.81):
     
-        line_1.set_data([0, x[i]], [0, y[i]])
-        circle.center = (x[i], y[i])
-        return line_1, circle # Passa að komman þarf að vera
-
-    anim = animation.FuncAnimation(
-        fig, animate, frames=len(theta), interval=1000/FPS_PLAY,
-        blit=True, init_func=init, repeat=False
-    )
-    plt.show()
-
-    return anim
-
-
-
-def save_animation(anim, filename="vid.mp4", fps=30):
-    
-    choice = input("Make video file? (Y/N): ").strip().lower()
-    if choice == "y":
-        Writer = animation.writers['ffmpeg']
-        writer = Writer(fps=fps, metadata=dict(artist="Me"), bitrate=1800)
-        anim.save(filename, writer=writer)
-        print("Saved to:", os.path.abspath(filename))
-    else:
-        print("Video not saved.")
-
-def mjaaa(start, m1=1, m2=1, l1=2, l2=2, g=9.81):
-    # start = [y1, y2, y3, y4] = [theta1, theta2, omega1, omega2]
+    # start = [y1, y2, y3, y4] = [θ1, θ2, ω1, ω2]
     y1 = start[0]
     y2 = start[1]
     y3 = start[2]
     y4 = start[3]
 
-    delta = y2-y1 # Δ = theta2 - theta1 = y2 - y1
+    delta = y2-y1 # Δ = θ2 - θ1
 
     a = m2*l1*(y3**2)*m.sin(delta)*m.cos(delta)
     b = m2*g*m.sin(y2)*m.cos(delta)
@@ -145,21 +76,23 @@ def mjaaa(start, m1=1, m2=1, l1=2, l2=2, g=9.81):
     k = (m1+m2)*l2
     l = m2*l2*((1-m.cos(2*delta))/2)
     func2 = (-i+j)/(k-l)
-    return [y3, y4, func1, func2]
+
+    return np.array([y3, y4, func1, func2]) 
 
 
-def RKsolverLotkaVolterra_Y4(y0, T, n, f): # modified for vector y 4x1
+def RKsolver_Y4(y0, T, n, f, y_final=''): # modified for vector y 4x1
 
-    y = np.array(y0, dtype=float)
     h = T / n  # time step
 
-    # y0 = [y1, y2, y3, y4] = [theta1, theta2, omega1, omega2]
+    y = y0
+
+    # y0 = [y1, y2, y3, y4] = [θ1, θ2, ω1, ω2]
     y1 = [y0[0]]
     y2 = [y0[1]]
     y3 = [y0[2]]
     y4 = [y0[3]]
 
-    for i in range(n):
+    for _ in range(n):
         k1 = f(y)
         k2 = f(y + 0.5*h*k1)
         k3 = f(y + 0.5*h*k2)
@@ -170,8 +103,89 @@ def RKsolverLotkaVolterra_Y4(y0, T, n, f): # modified for vector y 4x1
         y2.append(y[1])
         y3.append(y[2])
         y4.append(y[3])
+    if y_final == 'y':
+        return np.array(y1[-1], y2[-1], y3[-1], y4[-1])
+    else:
+        return np.array([y1, y2, y3, y4]) # θ1:list, θ2:list, ω1:list, ω2:list
 
-    return y1, y2, y3, y4 # theta1:list, theta2:list, omega1:list, omega2:list
+
+def make_plt(pendulums, theta, filename='vid.mp4',fps=30):
+
+    num_p = len(pendulums)  # 1 or 2
+
+
+    plt.close("all")
+    fig = plt.figure()
+    ax = fig.add_subplot(
+        111, autoscale_on=False,
+        xlim=(-5, 5), ylim=(-5, 5),
+        aspect="equal"
+    )
+    plt.xlabel("Lengdareining")
+    plt.ylabel("Lengdareining")
+    fig.tight_layout()
+
+    FPS_PLAY = 30
+
+    line_objects = []
+    circle_objects = []
+
+    # PENDULUM 1 
+    x1, y1 = pendulums[0]
+    line1, = ax.plot([0, x1[0]], [0, y1[0]], "b-", lw=2)
+    bob1 = ax.add_patch(plt.Circle((x1[0], y1[0]), 0.1, fc="r", zorder=3))
+
+    line_objects.append(line1)
+    circle_objects.append(bob1)
+
+    # PENDULUM 2 
+    if num_p == 2:
+        x2, y2 = pendulums[1]
+        line2, = ax.plot([x1[0], x2[0]], [y1[0], y2[0]], "b-", lw=2)
+        bob2 = ax.add_patch(plt.Circle((x2[0], y2[0]), 0.1, fc="r", zorder=3))
+
+        line_objects.append(line2)
+        circle_objects.append(bob2)
+
+
+    def init():
+        for line in line_objects:
+            line.set_data([], [])
+        for circ in circle_objects:
+            circ.center = (-1000, -1000)
+        return line_objects + circle_objects
+
+
+    def animate(i):
+
+
+        x1_i, y1_i = x1[i], y1[i]
+        line1.set_data([0, x1_i], [0, y1_i])
+        bob1.center = (x1_i, y1_i)
+
+        if num_p == 2:
+            x2_i, y2_i = x2[i], y2[i]
+            line2.set_data([x1_i, x2_i], [y1_i, y2_i])
+            bob2.center = (x2_i, y2_i)
+
+        return line_objects + circle_objects
+
+    anim = animation.FuncAnimation(
+        fig, animate, frames=len(theta),
+        interval=1000/FPS_PLAY, blit=True,
+        init_func=init
+    )
+
+    plt.show()
+
+    choice = input("Make video file? (Y/N): ").strip().lower()
+    if choice == "y":
+        Writer = animation.writers["ffmpeg"]
+        writer = Writer(fps=fps, metadata=dict(artist="Me"), bitrate=1800)
+        anim.save(filename, writer=writer)
+        print("Saved to:", filename)
+    else:
+        print("Video not saved.")
 
 def omegaDot(theta1, theta2, omega1, omega2, m1=1, m2=1, L1=2, L2=2, g=9.81):
     delta = theta2 - theta1
