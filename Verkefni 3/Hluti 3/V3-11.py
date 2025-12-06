de7f5fbfe904616604c0d84210afd099b15b39a2
JBTmarel <a.orribjarnason@gmail.com>
2025-12-06 14:53:31 +0000

    dæmi 11

--- Verkefni 3/Hluti 3/V3-11.py
+++ Verkefni 3/Hluti 3/V3-11.py
@@ -6,7 +6,7 @@ def makeDoublePendulumCoords(y0, T, n, l1=2, l2=2):
     y0=[theta1_0, theta2_0, omega1_0, omega2_0]
     '''
 
-    theta1, theta2, omega1, omega2 = f.RKsolverLotkaVolterra_Y4(y0, T, n, f.mjaaa)
+    theta1, theta2, omega1, omega2 = f.RKsolver_Y4(y0, T, n, f.mjaaa)
     theta_both = [theta1,theta2]
 
     x1 = [l1*m.sin(theta) for theta in theta1]
@@ -17,11 +17,14 @@ def makeDoublePendulumCoords(y0, T, n, l1=2, l2=2):
     #f.make_plt([(x1, y1),(x2, y2)], theta1, filename, fps)
 
 
-epsilon = 10**(-4)
-y10 = [m.pi/2, m.pi/2, 0, 0]
-y20 = [epsilon + m.pi/2, epsilon + m.pi/2, 0, 0]
+epsilons = [3, 5, 8]
 
-pendulum1 = makeDoublePendulumCoords(y10, 120, 5000)
-pendulum2 = makeDoublePendulumCoords(y20, 120, 5000)
+for x in epsilons:
+    epsilon = 10**(-x)
+    y10 = [m.pi/2, m.pi/2, 0, 0]
+    y20 = [epsilon + m.pi/2, epsilon + m.pi/2, 0, 0]
 
-f.make_multi_pendulums_plt([pendulum1, pendulum2], pendulum1[0][0], filename='multi_pendulum.mp4', fps=30)
+    pendulum1 = makeDoublePendulumCoords(y10, 120, 5000)
+    pendulum2 = makeDoublePendulumCoords(y20, 120, 5000)
+
+    f.make_multi_pendulums_plt([pendulum1, pendulum2], pendulum1[0][0], filename='V3-11_{}.mp4'.format(x), fps=30)
