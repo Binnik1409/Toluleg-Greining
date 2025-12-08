1c1964991ab5c23967aeb2a2534bf4701fac1934
Thorsteinn Gretar Garðarsson <Thorsteinngretar@email.is>
2025-12-08 16:52:53 +0000

    Fín pússun

--- Upphitun Vika 3/U-4.py
+++ Upphitun Vika 3/U-4.py
@@ -9,22 +9,9 @@ h = L/(n-1)
 
 x = np.linspace(0, np.pi/2, n)
 
-# row1 = np.zeros((1,n))
-# row1[0][0] = -3-2*h
-# row1[0][1] = 4
-# row1[0][2] = -1
-
-# row2 = np.zeros((1,n))
-# row2[0][-3] = -1
-# row2[0][-2] = 4
-# row2[0][-1] = -3-4*h
-
-
-# print(row2)
 
 alpha = -2 + h**2
 print(h)
-
 
 
 diag1 = np.diag([1 for i in range(n-1)], -1)
