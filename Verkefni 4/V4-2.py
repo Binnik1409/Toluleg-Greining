4a07840c8cd8ee84b3da5e384d8cf838d9092fd6
thefinaltry <finnurhugi@gmail.com>
2025-12-09 13:43:55 +0000

    rew

--- Verkefni 4/V4-2.py
+++ Verkefni 4/V4-2.py
@@ -1,4 +1,17 @@
+import numpy as np
 
 
 
+### Fastar ###
+Lx = Ly = 2
+L = 5
+delta = 0.1
+P = 5
+K = 1.68
+H = 0.005
+# Upphafshitastig er 20 gráður Celsíus
+# Jöfnurnar gera ráð fyrir hitastigi 0 gráður Celsíus
+# Því þarf að bæta 20 gráðum við niðurstöðuna til að fá rétta lausn
+Upphafshitastig = 20
 
+m = n = 10
---
+++ Verkefni 4/functions.py
@@ -0,0 +1,24 @@
+import functions as f
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def make_matrix(n,m,Lx,Ly,L):
+
+    dx = Lx / n
+    dy = Ly / m
+
+    matrix = np.zeros((n,m))
+    for m,j in enumerate(range(0,Ly,dy)):
+        for n,i in enumerate(range(0,Lx,dx)):
+
+            if i <= Lx/2 and j <= L: # vinstra megin, neðri hluti
+                ...
+            elif i <= Lx/2 and j > L: # vinstra megin, efri hluti
+                ...
+            elif i > Lx/2 and j >= Ly/2: # hægra megin, efri hluti
+                ...
+            else: # hægra megin, neðri hluti
+                ...
+                
+    return matrix
