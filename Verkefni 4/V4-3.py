e345f8c5662835704b15a0f883b43fb23b6e38db
Binnik1409 <brynjarh23@ru.is>
2025-12-10 13:46:32 +0000

    ljkj

--- Verkefni 4/V4-3.py
+++ Verkefni 4/V4-3.py
@@ -1,4 +1,9 @@
+import scipy.sparse as sp
+import numpy as np
 
+i = np.array([0,1,1,2,3,3,4])
+j = np.array([1,0,4,2,3,4,1])
+gildi = np.array([1,2,4,3,-1,1,1])
 
-
-
+a = sp.coo_matrix((gildi,(i,j)),shape=(5,5)).tocsr()
+print(a.toarray())
