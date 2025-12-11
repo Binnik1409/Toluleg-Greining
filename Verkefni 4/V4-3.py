0312df11d88f531e44a1e4b0b8d7330ef6dfb195
Binnik1409 <brynjarh23@ru.is>
2025-12-11 14:45:02 +0000

    asdf

--- Verkefni 4/V4-3.py
+++ Verkefni 4/V4-3.py
@@ -1,13 +1,6 @@
 import scipy.sparse as sp
 import numpy as np
 
-# i = np.array([0,1,1,2,3,3,4])
-# j = np.array([1,0,4,2,3,4,1])
-# gildi = np.array([1,2,4,3,-1,1,1])
-
-# a = sp.coo_matrix((gildi,(i,j)),shape=(5,5)).tocsr()
-# print(a.toarray())
-
 def ehhh(n, m, hx, hy, L=2, H=0.005, K=1.68, d=0.1, P=5):
     Ai = []
     Aj = []
