39f45e652228deda1dc570853c34e8f2ffb27aa9
Binnik1409 <brynjarh23@ru.is>
2025-12-11 14:42:58 +0000

    asdf

--- Verkefni 4/V4-3.py
+++ Verkefni 4/V4-3.py
@@ -1,9 +1,51 @@
 import scipy.sparse as sp
 import numpy as np
 
-i = np.array([0,1,1,2,3,3,4])
-j = np.array([1,0,4,2,3,4,1])
-gildi = np.array([1,2,4,3,-1,1,1])
+# i = np.array([0,1,1,2,3,3,4])
+# j = np.array([1,0,4,2,3,4,1])
+# gildi = np.array([1,2,4,3,-1,1,1])
 
-a = sp.coo_matrix((gildi,(i,j)),shape=(5,5)).tocsr()
-print(a.toarray())
+# a = sp.coo_matrix((gildi,(i,j)),shape=(5,5)).tocsr()
+# print(a.toarray())
+
+def ehhh(n, m, hx, hy, L=2, H=0.005, K=1.68, d=0.1, P=5):
+    Ai = []
+    Aj = []
+    Agildi = []
+    Bi = []
+    Bgildi = []
+    for k in range(n*m):
+        if k % n == 0:
+            if k * hy > L:
+                Ai += [k]*3
+                Aj += [k+2, k+1, k]
+                Agildi += [-1, 4, ((2*H*hx)/K)-3]         
+            else:
+                Ai += [k]*3
+                Aj += [k, k+1, k+2]
+                Agildi += [3, -4, 1]
+                Bi += [k]
+                Bgildi += [(-2*P*hx)/(L*d*K)]
+        elif k % n == n-1:
+            Ai += [k]*3
+            Aj += [k-2, k-1, k]
+            Agildi += [-1, 4, ((2*H*hx)/K)-3]
+        elif k // n == 0:
+            Ai += [k]*3
+            Aj += [k, k+m, k+2*m]
+            Agildi += [((2*H*hx)/K)-3, 4, -1]
+        elif k // n == m-1:
+            Ai += [k]*3
+            Aj += [k, k-m, k-2*m]
+            Agildi += [((-2*H*hx)/K)-3, 4, -1]
+        else:
+            Ai += [k]*5
+            Aj += [k-m, k-1, k, k+1, k+m]
+            Agildi += [hx**2, hy**2, ((2*H*(hx**2)*(hy**2))/(K*d))-4, hy**2, hx**2]
+    return Ai, Aj, Agildi, Bi, Bgildi
+
+Ai, Aj, Agildi, Bi, Bgildi = ehhh(5, 5, 1, 1)
+A = sp.coo_matrix((Agildi,(Ai,Aj)),shape=(5*5,5*5)).tocsr()
+print(A.toarray())
+
+
